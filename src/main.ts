1c8824f803682f1a92b41e81e042d04f16cd70a9
vabrosnet <vabros@hotmail.com>
2023-11-01 22:27:06 -0300

    db

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'process';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, hostname: '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
