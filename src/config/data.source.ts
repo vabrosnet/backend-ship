0d669aca65b7ce341175aef2911686e6e6ce49e0
vabrosnet <vabros@hotmail.com>
2023-11-02 13:57:01 -0300

    variables de entorno

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -12,11 +12,11 @@ const configService = new ConfigService();
 
 export const DataSourceConfig: DataSourceOptions = {
     type: 'mysql',
-    host: configService.get('DB_HOST'),
-    port: configService.get('DB_PORT'),
-    username: configService.get('DB_USER'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
+    host: configService.get('MYSQLHOST'),
+    port: configService.get('MYSQLPORT'),
+    username: configService.get('MYSQLUSER'),
+    password: configService.get('MYSQL_ROOT_PASSWORD'),
+    database: configService.get('MYSQLDATABASE'),
     entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
     synchronize: true,
